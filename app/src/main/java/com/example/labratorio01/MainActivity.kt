057ca8db65985165fdcdcafa900628b9b7d02320
Valeria Estrada <er2009012020@unab.edu.sv>
2022-10-06 22:51:09 -0600

    Initial commit

---
+++ app/src/main/java/com/example/labratorio01/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.labratorio01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.core.view.get
+
+class MainActivity : AppCompatActivity() {
+    lateinit var etnNombre: EditText
+    lateinit var etnEdad: EditText
+    lateinit var etnDepartamento: EditText
+    private var tvresultado: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        etnNombre = findViewById(R.id.etn_nombre)
+        etnEdad = findViewById(R.id.etn_edad)
+        etnDepartamento = findViewById(R.id.etn_departamento)
+        tvresultado = findViewById(R.id.tv_seleccion)
+
+    }
+
+    fun ingresar(Vista: View) {
+        val nombre = etnNombre.text.toString()
+        val edad = etnEdad.text.toString()
+        val departamento = etnDepartamento.toString()
+        var resultado = tvresultado?.text.toString()
+
+
+        if (nombre.length == 0) {
+            Toast.makeText(this, "Ingresar nombre", Toast.LENGTH_LONG).show()
+        }
+        if (edad.length == 0) {
+            Toast.makeText(this, "Ingresar edad", Toast.LENGTH_LONG).show()
+        }
+        if (departamento.length == 0) {
+            Toast.makeText(this, "Ingresar departamento", Toast.LENGTH_LONG).show()
+        }
+        if (nombre.length != 0 && edad.length != 0 && departamento.length != 0) {
+            Toast.makeText(this, "Registro en proceso...", Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            resultado = "Su nombre es ${nombre} de ${edad} del departamento de ${departamento}"
+        }
+    }
+}
+
+
+
+
+
